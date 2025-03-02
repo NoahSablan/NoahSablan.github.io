a85c2c8d51f7468fb1cbf84b14f518590d657816
NoahSablan <noah.a.sablan@gmail.com>
2025-03-01 22:49:04 -0500

    Contact page, dynamic nav bar, snapping on home pa ge, and some css cleanup, uniorm images on pages.

---
+++ static/navbar.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const navbarHTML = `
+      <nav class="navbar navbar-expand-sm navbar-light bg-light fixed-top">
+        <a class="navbar-brand" href="./index.html">Noah Sablan</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbarXs">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="navbar-collapse collapse" id="collapsingNavbarXs">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="./index.html">Projects</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="./contact-me.html">Contact</a>
+            </li>
+            <!--
+            <li class="nav-item">
+              <a class="nav-link" href="../arch/">Architecture</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../product/">Product</a>
+            </li> -->
+          </ul>
+        </div>
+      </nav>
+    `;
+    
+    // Insert at the beginning of the body
+    document.body.insertAdjacentHTML('afterbegin', navbarHTML);
+  });

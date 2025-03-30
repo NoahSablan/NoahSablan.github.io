06533fad74706add756d2613ee9c4b99b3f136ac
NoahSablan <noah.a.sablan@gmail.com>
2025-03-30 18:28:22 -0400

    Adding Noguchi contest.

--- static/home.js
+++ static/home.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
       
       sections.forEach((section, index) => {
           const background = section.querySelector('.background');
-          const theme = ["../3D/media/NN-1.jpg", "../3D/media/quilt-2.jpg", "../arch/media/newburgh-2.JPG", "../3D/media/VRT-showcase.png", "../arch/media/flood-1.JPG", "../ui-ux/media/ambient-4.png", "../3D/media/dml-2.gif"][index % 7];
+          const theme = ["../3D/media/NN-1.jpg", "../3D/media/quilt-2.jpg", "../3D/media/Noguchi-7.png", "../arch/media/newburgh-2.JPG", "../3D/media/VRT-showcase.png", "../arch/media/flood-1.JPG", "../ui-ux/media/ambient-4.png", "../3D/media/dml-2.gif"][index % 7];
           
           if (width < 600) {
               // Small screens (mobile)
